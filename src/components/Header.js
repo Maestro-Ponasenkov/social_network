d12ce410082d3e6f874f92781ccea97d80bbea9b
Дима Харитоник <haritonickd@gmail.com>
2021-10-17 16:44:26 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://i.imgur.com/AzjqDEi.png' alt='logo'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://i.imgur.com/NmzaCA9.jpg' alt='main-content'/>
+            </div>
+            <div>
+                <img className='avatar' src='https://i.imgur.com/OE5hM33.jpg' alt='avatar'/>
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
