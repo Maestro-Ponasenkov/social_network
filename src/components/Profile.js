0e945ed417dd129925ed8121448b27f962847f22
Maestro <haritonickd@gmail.com>
2021-10-17 18:22:53 +0300

    padding and styling Profiles

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Profile = () => {
     return (
         <div className='content'>
-            <div className='avatar'>
+            <div className='avatar-div'>
                 <img className='avatar' src='https://i.imgur.com/OE5hM33.jpg' alt='avatar'/>
             </div>
             <div className='posts'>
