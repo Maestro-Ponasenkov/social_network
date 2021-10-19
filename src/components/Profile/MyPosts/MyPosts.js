8e18e5240b4260f1473107aa957b7e5f3ba80a50
Maestro <haritonickd@gmail.com>
2021-10-19 15:17:44 +0300

    add like/dislike button and counter. Add post info

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,19 +3,22 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+    // let textarea = 0;
+    // let button = 0;
+
+    // button.addEventListener('click', Post());
     return (
             <div className={s.posts}>
                 My posts
                 <div>
-                    <textarea></textarea>
-                    <button>Add post</button>
+                    <textarea id='newPost'>ffasdfasdfasdfasdfasd</textarea>
+                    {/*{textarea = (document.getElementById('newPost').value)};*/}
+                    <button id='addPost'>Add post</button>
+                    {/*{button = (document.getElementById('addPost').value)};*/}
+
                 </div>
-               <Post />
-               <Post />
-               <Post />
-               <Post />
-               <Post />
-               <Post />
+               <Post message='Hi how are you?' likecount='15' dislikecount='2'/>
+               <Post message="It's my first post" likecount='20' dislikecount='5'/>
             </div>
     )
 }
--- src/components/Profile/MyPosts/Post/Post.js
+++ src/components/Profile/MyPosts/Post/Post.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div>
             <div className={s.item}>
-                Post 1
+                <img src='https://i.imgur.com/uPOwfbE.png' className={s.profile_image}/>
+                {props.message}
+            </div>
+
+            <div className={s.like_dislike}>
+                <span>
+                    <img src='https://i.imgur.com/3BWMVOy.png' alt='like' className={s.like_dislike_image}/>
+                </span>
+                {props.likecount}
+                <span>
+                    <img src='https://i.imgur.com/8UCDIll.png' alt='like' className={s.like_dislike_image}/>
+                </span>
+                {props.dislikecount}
             </div>
         </div>
     )
